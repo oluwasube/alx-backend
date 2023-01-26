395d0b9bdc17dd36326994b00441f2c7d2dbdb60
oluwasube <enibeke20@gmail.com>
2023-01-26 23:24:58 +0100

    task 0 completed

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''
+Basic Dictionary
+'''
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+
+    """ BasicCache class that inherits from BaseCaching
+    """
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
