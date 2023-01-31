5fdeb423baf246d31b1d09632e52a3d27a03273c
oluwasube <enibeke20@gmail.com>
2023-01-31 12:11:13 +0100

    task 0 successful

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home_index() -> str:
+    '''
+    The home index page
+    '''
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+    
