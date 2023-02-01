3fc2f9b43bd542c5c48dbf878d11a7f06840694b
oluwasube <enibeke20@gmail.com>
2023-02-01 10:59:26 +0100

    fixed trailing whitespace

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,6 +6,7 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home_index() -> str:
     '''
@@ -13,6 +14,6 @@ def home_index() -> str:
     '''
     return render_template('0-index.html')
 
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-    
