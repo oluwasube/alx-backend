f070bc4d370c030e2118b55e93dfb64870e7b382
oluwasube <enibeke20@gmail.com>
2023-02-01 15:41:03 +0100

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -6,18 +6,19 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-
 class Config(object):
     '''
     configuring Flask Babel
     '''
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE='en',
-    BABEL_DEFAULT_TIMEZONE='UTC'
+    BABEL_DEFAULT_LOCALE = 'en',
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale() -> str:
@@ -33,6 +34,7 @@ def home_index() -> str:
     The home index page
     '''
     return render_template('3-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
