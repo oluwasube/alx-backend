c2f59f470759a2dc2a1e495f455c9212b1a218af
oluwasube <enibeke20@gmail.com>
2023-02-01 14:57:12 +0100

    remvoe trailing whitespace

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -6,21 +6,25 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-
 class Config(object):
     '''
     configuring Flask Babel
     '''
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE='en',
-    BABEL_DEFAULT_TIMEZONE='UTC'
+    BABEL_DEFAULT_LOCALE = 'en',
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
 babel = Babel(app)
 app.config.from_object(Config)
 
+
 @babel.localeselector
 def get_locale() -> str:
+    '''
+    Select and return best language match based on supported languages
+    '''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
@@ -30,6 +34,7 @@ def home_index() -> str:
     The home index page
     '''
     return render_template('2-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
