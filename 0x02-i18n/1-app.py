0cc98aee5c78f30ae39171973e7f59c43778f156
oluwasube <enibeke20@gmail.com>
2023-02-01 11:06:43 +0100

    fixed pycodestyle

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -6,19 +6,18 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-
 class Config(object):
     '''
     configuring Flask Babel
     '''
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE='en',
-    BABEL_DEFAULT_TIMEZONE='UTC'
+    BABEL_DEFAULT_LOCALE = 'en',
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app = Flask(__name__)
-babel = Babel(app)
 app.config.from_object(Config)
-
+babel = Babel(app)
 
 
 @app.route('/', strict_slashes=False)
@@ -27,6 +26,7 @@ def home_index() -> str:
     The home index page
     '''
     return render_template('1-index.html')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
