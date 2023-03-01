4cfd2ebc5a58a391b604a061cbdf941591bec7d3
oluwasube <enibeke20@gmail.com>
2023-03-01 18:52:59 +0100

    task completed

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -4,12 +4,13 @@ import { createClient } from 'redis';
 function connectToRedis() {
     const client = createClient();
 
-    client.on('connect', function() {
-      console.log('Redis client connected to the server');
-    }).on('error', (err) => {
-      console.log(`Redis client not connected to the server: ${err}`);
-    });
+    client.on('connect', () => {
+        console.log('Redis client connected to the server');
+      });
+    
+      client.on('error', (err) => {
+        console.log(`Redis client not connected to the server: ${err.message}`);
+      });
 };
 
 connectToRedis();
-
